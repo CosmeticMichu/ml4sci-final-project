56560b50b7e6965258e7579b9d652cb0613efe2d
CosmeticMichu <eariasp@unal.edu.co>
2025-12-13 19:51:06 -0500

    mofified files for development
    supressed version of run.sh and models with a lot of commented lines and extralines with model implementations

--- Romualdi-Marchetti/src/run.sh
+++ Romualdi-Marchetti/src/run.sh
@@ -1,18 +1,22 @@
 cd double_yukawa
+echo "Training for Double Yukawa potential"
 python train.py
 
-cd ..
-cd yukawa
-python train.py
+# cd ..
+# cd yukawa
+# echo "Training for Yukawa potential"
+# python train.py
 
-cd ..
-cd exp_cosine
-python train.py
+# cd ..
+# cd exp_cosine
+# echo "Training for Exp Cosine potential"
+# python train.py
 
-cd ..
-cd square_well
-python train.py
+# # cd ..
+# # cd square_well
+# # python train.py
 
-cd ..
-cd yukawa
-python train.py
+# # cd ..
+# # cd yukawa
+# # echo "Training for Yukawa potential (again)"
+# # python train.py
